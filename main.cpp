8c42089ed87727c4ecedf2bb2e292e071ff5602d
vladosflux <viperviperiv@gmail.com>
2025-11-19 16:32:40 +0200

    Finish playGame function

--- main.cpp
+++ main.cpp
@@ -6,6 +6,15 @@
 #include <limits>
 using namespace std;
 
+void optionOutput(){
+    cout << R"(Menu:
+    1: Add New Word
+    2: Play Game
+    0: Quit
+
+    You pick: )";
+}
+
 void cleanCin(){
     cin.clear();
     cin.ignore(numeric_limits<streamsize>::max(),'\n');
@@ -20,8 +29,10 @@ void wordInput(string& word) {
         try {
             cin.ignore(numeric_limits<streamsize>::max(),'\n');
             cout << "> ";
+            
             getline(cin, word);
-
+            if(word.empty()) continue;
+            
             for (size_t i = 0; i < word.size(); i++) {
                 if (bannedSymbols.find(word[i]) != string::npos) {
                     throw std::runtime_error("The word must not contain numeric characters, symbols, or spaces");
@@ -35,33 +46,65 @@ void wordInput(string& word) {
 }
 
 void playGame(string& word){
+    if(word.empty()){
+        cout << "\033[3J\033[H\033[2J";
+        cout << "THE WORD MUST NOT BE EMPTY\n\n";
+        return;
+    }
+    
     char charToGuess;
     vector<char> foundChars;
-    int br = 0;
+    int maxWrong = 0;
     string wordOutput(word.size(), '_');
 
-    while (br < 8) {
+    while (true) {
        cout << "Take a guess!\n>";
        bool found = false;
+  
+
        cin >> charToGuess;
-       cleanCin();
+       cleanCin();  
+       
        for(size_t i = 0; i < word.size();i++){
+       
         if(word[i] =='0') continue;
+
+
         if(word[i] == charToGuess ){
             found = true;
             wordOutput[i] = charToGuess;
             word[i] = '0';
+
             foundChars.push_back(charToGuess);
         }
        }
+  
+       if(wordOutput.find('_') == string::npos){
+        cout << "\033[3J\033[H\033[2J";
+        cout << "YOU WON!!!\n";
+        cout << "\n";
+        
+        return;      
+       }
+
        if(!found){
-        br++;
+        maxWrong++;
+            if(maxWrong == 8){
+            cout << "You lost!\n";
+            cout << "\033[3J\033[H\033[2J";
+            return;   
+        }    
         cout << "There is no \'" << charToGuess << "\' in the word" << endl;
        }
+       
        for(size_t i = 0; i < wordOutput.size();i++ )
        {
         cout << wordOutput[i];
        }
+       
+       
+
+       found = false;
        cout << "\n";
     }
 }
@@ -94,24 +137,33 @@ int main(){
             You pick: )";
 
     int option;
-    while(true){
-    cin >> option;
-    switch (option) {
-        case 1:wordInput(word);break;
-        
-        case 2:playGame(word);break;
-        
-        case 0: return 0;
-        
-        default: cout << "Invalid option";
-            
-    }
-    cout << R"(Menu:
-            1: Add New Word
-            2: Play Game
-            0: Quit
+    while (true) {
 
-            You pick: )";
+   
+
+    cin >> option;
+
+    switch (option) {
+        case 1:
+            wordInput(word);
+            break;
+
+        case 2:
+            playGame(word);
+            
+            word = "";
+            break;
+
+        case 0:
+            return 0;
+
+        default:
+            cout << "Invalid option\n";
+            break;
+        
+    }
+    optionOutput();
 }
+
 
 }
